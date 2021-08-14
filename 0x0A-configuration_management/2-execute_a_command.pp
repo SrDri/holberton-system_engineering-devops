47322854b4e8d3ecc859e6c34afc7f667752d028
SrDri <juanjcarabali@gmail.com>
2021-08-14 07:22:46 +0700

    Fix update project

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
 # Task 2 - sing Puppet, create a manifest that kills a process named killmenow.
-
 exec { 'killmenow':
 	command => 'pkill -f killmenow',
 	provider => 'shell',
-	path    => '/usr/bin'
+	path => '/usr/bin'
 }
