934dfbd59af7590d44eaebf3efaa305966908bd2
SrDri <juanjcarabali@gmail.com>
2021-09-14 02:38:02 +0700

    Update task 3 and update

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Export CSV API """
+""" Export JSON API """
 import json
 from requests import get
 from sys import argv
---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Export JSON ALL API """
+import json
+from requests import get
+from sys import argv
+
+
+def all_tasks():
+    """ Return employee info TODO list """
+
+    filename = "todo_all_employees.json"
+    API = "https://jsonplaceholder.typicode.com/"
+
+    employees = get(API + "users/").json()
+    todos = get(API + "todos/").json()
+
+    struct = {}
+    for employe in employees:
+        one_id = employe.get("id")
+        struct[one_id] = []
+        for one in todos:
+            one_username = one.get("username")
+            one_task_status = one.get("completed")
+            one_task_title = one.get("title")
+            struct[one_id].append({"username": one_username,
+                                   "task": one_task_title,
+                                   "completed": one_task_status})
+
+    with open(filename, "w") as f:
+        json.dump(struct, f)
+
+
+if __name__ == "__main__":
+    all_tasks()
