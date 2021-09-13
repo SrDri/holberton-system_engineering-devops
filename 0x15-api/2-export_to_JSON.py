9c0c4cf4f4b10c18436c83a0996ad98cf3935bdd
SrDri <juanjcarabali@gmail.com>
2021-09-14 02:11:18 +0700

    Update task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Export CSV API """
+import json
+from requests import get
+from sys import argv
+
+
+def tasks_list(employee_id=None):
+    """ Return employee info TODO list """
+
+    filename = employee_id + ".json"
+    API = "https://jsonplaceholder.typicode.com/"
+
+    try:
+        employee_id = int(employee_id)
+    except:
+        return "No valid employee id"
+
+    employee = get(API + "users/{}". format(employee_id)).json()
+    todos = get(API + "todos?userId={}".format(employee_id)).json()
+
+    _id = int(employee_id)
+    username = employee.get("username")
+
+    struct = {employee_id: []}
+    for task in todos:
+        task_title = task.get("title")
+        task_status = task.get("completed")
+        struct[employee_id].append({"task": task_title,
+                                    "completed": task_status,
+                                    "username": username})
+
+    with open(filename, "w") as f:
+        json.dump(struct, f)
+
+
+if __name__ == "__main__":
+    tasks_list(argv[1])
